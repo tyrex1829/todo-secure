1c9ff90044406b69a53894911a42066fc5e03b08
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-29 08:55:08 +0530

    server routing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,32 @@
 import express, { Application, Request, Response } from "express";
+import mongoose from "mongoose";
 import env from "dotenv";
 env.config();
+import cors from "cors";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// DB Connection
+
+// app.use("api/auth", authRoutes);
+
+const mongo_url =
+  "";
+
+mongoose
+  .connect(mongo_url, {
+    dbName: "auth-system-db",
+  })
+  .then(() => {
+    console.log(`DB connected`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get("/", (req: Request, res: Response): any => {
   return res.status(200).json({
